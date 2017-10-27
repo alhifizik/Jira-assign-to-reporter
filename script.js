9e8f4e8cf86407dca8b2e29b1aa168a3860af446
Andrei Suvorov <asuvorov@Exadels-MacBook-Pro-2.local>
2017-10-27 16:57:15 +0300

    almost implemented known issue: reporter and assignee are determined only after 2nd click

--- script.js
+++ script.js
@@ -41,42 +41,42 @@ function addElement() {
 function obtainInfo(issueKey, type) {
   restUrl = $(location).attr('href').substring(0,
     $(location).attr('href').indexOf('browse'))
-  restUrl += 'rest/api/latest/issue/' + issueKey + '?fields=' + type
-
+  restUrl += 'rest/api/latest/issue/' + issueKey + '?fields=reporter,assignee'
   var xmlHttp = new XMLHttpRequest()
   xmlHttp.onload = function() {
     response = xmlHttp.responseText
     obj = JSON.parse(response)
-    if (type == 'reporter'){
-      reporter = obj.fields.reporter.name
-    } else if (type == 'assignee') {
-      assignee = obj.fields.assignee.name
-    }
+    childReporter = obj.fields.reporter.name
+    childAssignee = obj.fields.assignee.name
   }
   xmlHttp.open( "GET", restUrl, true)
   xmlHttp.send()
-  return [reporter, assignee]
+  return [childReporter, childAssignee]
 }
 
 
-function contextmenu(){
-  assignItemLink = $('.aui-list-item-link.issueaction-assign-issue')
+function contextmenu(assignItemLink){
   issueKey = assignItemLink.attr('data-issuekey')
   issueId = assignItemLink.attr('data-issueid')
-  reporter = obtainInfo(issueKey, 'reporter')[0]
-  assignee = obtainInfo(issueKey, 'assignee')[1]
+  reporter = obtainInfo(issueKey)[0]
+  assignee = obtainInfo(issueKey)[1]
+  console.log(reporter)
+  console.log(assignee)
 
   if (reporter != assignee && reporter != currentUser) {
+
     li = $('<li>', {
       class: 'aui-list-item'
     }).insertAfter($('.aui-list-item-link.issueaction-assign-issue').parent())
+
     link = $('<a>', {
-      class: 'aui-list-item-link issueaction-assign-to-reporter',
-      href: "/secure/AssignIssue.jspa?atl_token=" + token + '&id=' + id +
+      href: "/secure/AssignIssue.jspa?atl_token=" + token + '&id=' + issueId +
       '&assignee=' + reporter + '&returnUrl=' + $(location).attr('href') +
       '&atl_token=' + token,
+      class: 'aui-list-item-link issueaction-assign-to-reporter',
       text: linkText
     }).appendTo(li)
+
 
     li.hover(function() {
       $(this).prop('class', 'aui-list-item active')
@@ -96,12 +96,12 @@ if (browse > 0 || issues > 0 || selectedIssue > 0) {
     if (link.length == 0 && !!$('[id^="issue_summary_reporter_"]')) {
       addElement()
     }
-    contextMenu = $('[role = "menu"]')
-    assignItemLink = $('.aui-list-item-link.issueaction-assign-issue')
-    reporterItemLink = $('.aui-list-item-link.issueaction-assign-to-reporter')
-    if (contextMenu.length == 1 && assignItemLink.length == 1 &&
-      reporterItemLink.length == 0) {
-      contextmenu()
+
+    popup = $('.ajs-layer.box-shadow.active')
+    assignItemLink = popup.find('.aui-list-item-link.issueaction-assign-issue')
+    reporterItemLink = popup.find('.aui-list-item-link.issueaction-assign-to-reporter')
+    if (popup.length > 0 && assignItemLink.length > 0 && reporterItemLink.length == 0) {
+      contextmenu(assignItemLink)
     }
 
   }
