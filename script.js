0d7d532472328510ef8f2ce178ab972a499d89b7
Andrei Suvorov <asuvorov@Exadels-MacBook-Pro-2.local>
2017-10-30 12:25:24 +0300

    remove unstable contextMenu functionality

--- script.js
+++ script.js
@@ -38,56 +38,6 @@ function addElement() {
   }
 }
 
-function obtainInfo(issueKey, type) {
-  restUrl = $(location).attr('href').substring(0,
-    $(location).attr('href').indexOf('browse'))
-  restUrl += 'rest/api/latest/issue/' + issueKey + '?fields=reporter,assignee'
-  var xmlHttp = new XMLHttpRequest()
-  xmlHttp.onload = function() {
-    response = xmlHttp.responseText
-    obj = JSON.parse(response)
-    childReporter = obj.fields.reporter.name
-    childAssignee = obj.fields.assignee.name
-  }
-  xmlHttp.open( "GET", restUrl, true)
-  xmlHttp.send()
-  return [childReporter, childAssignee]
-}
-
-function contextmenu(assignItemLink){
-  issueKey = assignItemLink.attr('data-issuekey')
-  issueId = assignItemLink.attr('data-issueid')
-  reporter = obtainInfo(issueKey)[0]
-  assignee = obtainInfo(issueKey)[1]
-  console.log(reporter)
-  console.log(assignee)
-
-  if (reporter != assignee && reporter != currentUser) {
-
-    li = $('<li>', {
-      class: 'aui-list-item'
-    }).insertAfter($('.aui-list-item-link.issueaction-assign-issue').parent())
-
-    link = $('<a>', {
-      href: "/secure/AssignIssue.jspa?atl_token=" + token + '&id=' + issueId +
-      '&assignee=' + reporter + '&returnUrl=' + $(location).attr('href') +
-      '&atl_token=' + token,
-      class: 'aui-list-item-link issueaction-assign-to-reporter',
-      text: linkText
-    }).appendTo(li)
-
-
-    li.hover(function() {
-      $(this).prop('class', 'aui-list-item active')
-      $(this).prev().prop('class', 'aui-list-item')
-      $(this).next().prop('class', 'aui-list-item')
-    }, function() {
-      $(this).prop('class', 'aui-list-item')
-    })
-  }
-}
-
-
 if (browse > 0 || issues > 0 || selectedIssue > 0) {
   $(document).keypress(function(){
     if(event.which == 114) {
@@ -99,13 +49,6 @@ if (browse > 0 || issues > 0 || selectedIssue > 0) {
     link = $('*[data-mydata="assign-to-reporter"]')
     if (link.length == 0 && !!$('[id^="issue_summary_reporter_"]')) {
       addElement()
-    }
-
-    popup = $('.ajs-layer.box-shadow.active')
-    assignItemLink = popup.find('.aui-list-item-link.issueaction-assign-issue')
-    reporterItemLink = popup.find('.aui-list-item-link.issueaction-assign-to-reporter')
-    if (popup.length > 0 && assignItemLink.length > 0 && reporterItemLink.length == 0) {
-      contextmenu(assignItemLink)
     }
   }
 
